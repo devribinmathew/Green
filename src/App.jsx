601f7557cff9c3b4c89e057116a63df4afb2d35a
ribinmathew <ribinmathew4u@gmail.com>
2023-12-18 18:23:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+function App() {
+  return (
+    <div>
+      <h1 className=' text-red-800  uppercase text-3xl m-20'>user login</h1>
+
+
+      {/* <div className="grid-flow  gap-7 bg-amber-400 m-32 "> */}
+
+      <div className="">
+
+      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+  <div>
+    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+      <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
+    </span>
+  </div>
+  <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
+  <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+  </p>
+</div>
+
+        <div className="bg-blue-600 h-10 w-5 ">
+        </div>
+
+        <div className="bg-black h-10 w-5 ">
+        </div>
+
+        <div className="bg-black h-10 w-5 ">
+        </div>
+
+
+        <div className="bg-black h-10 w-5 ">
+        </div>
+
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
